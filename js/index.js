e17adcb7abfc4ed16e033f43d55a146e6ac984c7
Michael Ergette <michael_e96@hotmail.co.uk>
2023-12-12 21:42:44 +0000

    placed javascript file in the correct folder

---
+++ js/index.js
@@ -0,0 +1,162 @@
+var finances = [
+  ["Jan-2010", 867884],
+  ["Feb-2010", 984655],
+  ["Mar-2010", 322013],
+  ["Apr-2010", -69417],
+  ["May-2010", 310503],
+  ["Jun-2010", 522857],
+  ["Jul-2010", 1033096],
+  ["Aug-2010", 604885],
+  ["Sep-2010", -216386],
+  ["Oct-2010", 477532],
+  ["Nov-2010", 893810],
+  ["Dec-2010", -80353],
+  ["Jan-2011", 779806],
+  ["Feb-2011", -335203],
+  ["Mar-2011", 697845],
+  ["Apr-2011", 793163],
+  ["May-2011", 485070],
+  ["Jun-2011", 584122],
+  ["Jul-2011", 62729],
+  ["Aug-2011", 668179],
+  ["Sep-2011", 899906],
+  ["Oct-2011", 834719],
+  ["Nov-2011", 132003],
+  ["Dec-2011", 309978],
+  ["Jan-2012", -755566],
+  ["Feb-2012", 1170593],
+  ["Mar-2012", 252788],
+  ["Apr-2012", 1151518],
+  ["May-2012", 817256],
+  ["Jun-2012", 570757],
+  ["Jul-2012", 506702],
+  ["Aug-2012", -1022534],
+  ["Sep-2012", 475062],
+  ["Oct-2012", 779976],
+  ["Nov-2012", 144175],
+  ["Dec-2012", 542494],
+  ["Jan-2013", 359333],
+  ["Feb-2013", 321469],
+  ["Mar-2013", 67780],
+  ["Apr-2013", 471435],
+  ["May-2013", 565603],
+  ["Jun-2013", 872480],
+  ["Jul-2013", 789480],
+  ["Aug-2013", 999942],
+  ["Sep-2013", -1196225],
+  ["Oct-2013", 268997],
+  ["Nov-2013", -687986],
+  ["Dec-2013", 1150461],
+  ["Jan-2014", 682458],
+  ["Feb-2014", 617856],
+  ["Mar-2014", 824098],
+  ["Apr-2014", 581943],
+  ["May-2014", 132864],
+  ["Jun-2014", 448062],
+  ["Jul-2014", 689161],
+  ["Aug-2014", 800701],
+  ["Sep-2014", 1166643],
+  ["Oct-2014", 947333],
+  ["Nov-2014", 578668],
+  ["Dec-2014", 988505],
+  ["Jan-2015", 1139715],
+  ["Feb-2015", 1029471],
+  ["Mar-2015", 687533],
+  ["Apr-2015", -524626],
+  ["May-2015", 158620],
+  ["Jun-2015", 87795],
+  ["Jul-2015", 423389],
+  ["Aug-2015", 840723],
+  ["Sep-2015", 568529],
+  ["Oct-2015", 332067],
+  ["Nov-2015", 989499],
+  ["Dec-2015", 778237],
+  ["Jan-2016", 650000],
+  ["Feb-2016", -1100387],
+  ["Mar-2016", -174946],
+  ["Apr-2016", 757143],
+  ["May-2016", 445709],
+  ["Jun-2016", 712961],
+  ["Jul-2016", -1163797],
+  ["Aug-2016", 569899],
+  ["Sep-2016", 768450],
+  ["Oct-2016", 102685],
+  ["Nov-2016", 795914],
+  ["Dec-2016", 60988],
+  ["Jan-2017", 138230],
+  ["Feb-2017", 671099],
+];
+
+for (let i =0; i< finances.length; i++){}
+var numberofmonths = finances.length;
+alert(`There are ${numberofmonths} months in this financial analysis`); //86 arrays? months
+
+// total profits:
+let totalprofits = 0;
+let totalchange = 0;
+let biggestchange = {date:"", amount:0}; //makes it an object so we can attach a key objecy: has a key and a value (more reliable/and for display)
+let biggestdecrease= {date: "", amount:0};
+for (let i = 0; i < finances.length; i++) {
+  let profits = finances[i][1]
+  totalprofits= totalprofits + profits
+  
+  //if statements
+
+
+  // for biggest change start with i=1
+  // let change = finances[i + 1][1] - finances[i][1];
+  // // console.log(finances[i][1],change)
+  // //console.log(change);
+  // totalchange = totalchange + change;
+  // //keep track of the biggest change
+  // if (change > biggestchange) {
+  //   let biggestchange = change;
+  //   console.log(biggestchange);
+ // }
+  //console.log(finances[i][1])
+ // let greatestincrease = 
+ if (i>0){
+  var change2= finances[i][1] - finances[i-1][1];
+  totalchange +=change2
+  if (change2>biggestchange.amount) {
+    biggestchange.amount=change2
+    biggestchange.date = finances[i][0]
+    
+  }
+  else if(change2<biggestdecrease.amount) {
+    biggestdecrease.amount=change2
+    biggestdecrease.date = finances[i][0]
+  }
+   
+}
+
+}
+let averagechange = totalchange / (numberofmonths - 1);
+
+console.log("Financial Analysis")
+console.log("---------------------")
+setTimeout(function(){
+  console.log("Total months:" + finances.length)
+  console.log("Total: " + "$"+ totalprofits)
+  console.log("Average change " + averagechange.toFixed(2))
+  console.log("Greatest increase in profits/losses "+ biggestchange.date, "$" + biggestchange.amount)
+  console.log("Greatest decrease in profits/losses: " + biggestdecrease.date, "$" + biggestdecrease.amount)
+}, 4000)
+
+
+//console.log(totalchange)
+
+
+//console.log(typeof finances[0][1])
+//variables:
+//total number of months
+//  rolling total of profits
+
+//  greatest increase month and Amt
+//  greatest loss
+//  average of changes
+
+//while loop/ for loop
+//inside the loop:
+//  current data Point
+//  previous datta point
